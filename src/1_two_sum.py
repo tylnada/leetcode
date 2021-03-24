86d4b2f7ae3892d67bef2240063828012d530448
Nada Guanlun Liang <35932785+tylnada@users.noreply.github.com>
2021-03-23 23:19:40 -0400

    Create 1_two_sum.py

---
+++ src/1_two_sum.py
@@ -0,0 +1,13 @@
+# brutal force
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if j != i:
+                    if nums[i] + nums[j] == target:
+                        return [i, j]
