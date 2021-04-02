f6c84f2a7000c278889681a16ea59f6caa8f1204
Nada Guanlun Liang <35932785+tylnada@users.noreply.github.com>
2021-04-01 22:29:17 -0400

    Update 98_validate_binary_search_tree.py

--- src/98_validate_binary_search_tree.py
+++ src/98_validate_binary_search_tree.py
@@ -1,8 +1,17 @@
-  #recursion  
-  def isValidBST(self, root, floor=float('-inf'), ceiling=float('inf')):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#recursion
+class Solution:
+    def isValidBST(self, root, floor=float('-inf'), ceiling=float('inf')):
         if not root:
             return True
         if root.val >= ceiling or root.val <= floor:
             return False
         # in the left branch, root is the new ceiling; contrarily root is the new floor in right branch
         return(self.isValidBST(root.left, floor, min(root.val, ceiling)) and self.isValidBST(root.right,max(root.val, floor), ceiling))
+
