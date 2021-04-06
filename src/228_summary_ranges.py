9a5c1b2876136e6c5616f730d14adc2b0566dc35
Nada Guanlun Liang <35932785+tylnada@users.noreply.github.com>
2021-04-05 22:45:00 -0400

    Update 228_summary_ranges.py

--- src/228_summary_ranges.py
+++ src/228_summary_ranges.py
@@ -30,6 +30,6 @@ class Solution:
         for n in nums:
             if n-1 not in r:
                 r = []
-                ranges += r,
-            r[1:] = n,
+                ranges.append(r)
+            r[1:] = [n]
         return ['->'.join(map(str, r)) for r in ranges]
